8af48f340935982c8c74f3c4a83dde610ebeba62
Michal Feder <michalf5655@gmail.com>
2021-03-11 10:23:13 +0200

    Add a solution way

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
+import statistics
 
 
-def print_avg():
+# first way: --complexity time o(n^2), complexity space o(1).
+def print_avg_1():
+    print('first way: --complexity time o(n^2), complexity space o(1)')
     with open('../Microsoft/Grades.json') as g:
         with open('../Microsoft/Students.json') as s:
             grades = json.load(g)
@@ -32,5 +35,42 @@ def print_avg():
                     print('subject: ', grade, ' avg: ', sum / count)
 
 
+# second way: --complexity time o(n), complexity space o(n^2).
+def print_avg_2():
+    print('second way: --complexity time o(n), complexity space o(n^2)')
+    with open('../Microsoft/Grades.json') as g:
+        with open('../Microsoft/Students.json') as s:
+            grades = json.load(g)
+            students = json.load(s)
+
+        my_map = {}
+
+        for grd in grades:
+            if my_map.get(grd['id']):
+                my_map[grd['id']].append(grd['grade'])
+            else:
+                my_map[grd['id']] = [grd['grade']]
+
+        print('Average for each student:')
+
+        for std in students:
+            if my_map.get(std['id']):
+                print('id: ', std['id'], ' name: ', std['name'], ' avg: ', statistics.mean(my_map.get(std['id'])))
+
+        my_map = {}
+        print('Average for each subject:')
+
+        for grd in grades:
+            if my_map.get(grd['subject']):
+                my_map[grd['subject']].append(grd['grade'])
+            else:
+                my_map[grd['subject']] = [grd['grade']]
+
+        for grade in list(dict.fromkeys([s['subject'] for s in grades])):
+            if my_map.get(grade):
+                print('subject: ', grade, ' avg: ', statistics.mean(my_map.get(grade)))
+
+
 if __name__ == '__main__':
-    print_avg()
+    print_avg_1()
+    print_avg_2()
